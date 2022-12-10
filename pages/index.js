44726dec7357d9f441e1951e6320921e116def62
nurulcse7 <nurul.cse7@gmail.com>
2022-12-10 19:21:32 +0600

    updated ProductItem

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,8 @@ export default function Home({ products }) {
   };
 
   return (
-    <Layout title="Home Page">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+    <Layout title='Home Page'>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
         {products.map((product) => (
           <ProductItem
             product={product}
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -14,7 +14,7 @@ export default function ProductScreen(props) {
   const { state, dispatch } = useContext(Store);
   const router = useRouter();
   if (!product) {
-    return <Layout title="Product Not Found">Product Not Found</Layout>;
+    return <Layout title='Product Not Found'>Product Not Found</Layout>;
   }
 
   const addToCartHandler = async () => {
@@ -32,23 +32,23 @@ export default function ProductScreen(props) {
 
   return (
     <Layout title={product.name}>
-      <div className="py-2">
-        <Link href="/">back to products</Link>
+      <div className='py-2'>
+        <Link href='/'>back to products</Link>
       </div>
-      <div className="grid md:grid-cols-4 md:gap-3">
-        <div className="md:col-span-2">
+      <div className='grid md:grid-cols-4 md:gap-3'>
+        <div className='md:col-span-2'>
           <Image
             src={product.image}
             alt={product.name}
             width={640}
             height={640}
-            layout="responsive"
+            layout='responsive'
           ></Image>
         </div>
         <div>
           <ul>
             <li>
-              <h1 className="text-lg">{product.name}</h1>
+              <h1 className='text-lg'>{product.name}</h1>
             </li>
             <li>Category: {product.category}</li>
             <li>Brand: {product.brand}</li>
@@ -59,17 +59,17 @@ export default function ProductScreen(props) {
           </ul>
         </div>
         <div>
-          <div className="card p-5">
-            <div className="mb-2 flex justify-between">
+          <div className='card p-5'>
+            <div className='mb-2 flex justify-between'>
               <div>Price</div>
               <div>${product.price}</div>
             </div>
-            <div className="mb-2 flex justify-between">
+            <div className='mb-2 flex justify-between'>
               <div>Status</div>
               <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
             </div>
             <button
-              className="primary-button w-full"
+              className='primary-button w-full'
               onClick={addToCartHandler}
             >
               Add to cart
