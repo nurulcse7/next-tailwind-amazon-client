b1b650e0772d95fb9e01303890ad3c6aab23f028
nurulcse7 <nurul.cse7@gmail.com>
2022-12-10 17:48:50 +0600

    added payment file

--- pages/shipping.js
+++ pages/shipping.js
@@ -50,82 +50,82 @@ export default function ShippingScreen() {
   };
 
   return (
-    <Layout title="Shipping Address">
+    <Layout title='Shipping Address'>
       <CheckoutWizard activeStep={1} />
       <form
-        className="mx-auto max-w-screen-md"
+        className='mx-auto max-w-screen-md'
         onSubmit={handleSubmit(submitHandler)}
       >
-        <h1 className="mb-4 text-xl">Shipping Address</h1>
-        <div className="mb-4">
-          <label htmlFor="fullName">Full Name</label>
+        <h1 className='mb-4 text-xl'>Shipping Address</h1>
+        <div className='mb-4'>
+          <label htmlFor='fullName'>Full Name</label>
           <input
-            className="w-full"
-            id="fullName"
+            className='w-full'
+            id='fullName'
             autoFocus
             {...register('fullName', {
               required: 'Please enter full name',
             })}
           />
           {errors.fullName && (
-            <div className="text-red-500">{errors.fullName.message}</div>
+            <div className='text-red-500'>{errors.fullName.message}</div>
           )}
         </div>
-        <div className="mb-4">
-          <label htmlFor="address">Address</label>
+        <div className='mb-4'>
+          <label htmlFor='address'>Address</label>
           <input
-            className="w-full"
-            id="address"
+            className='w-full'
+            id='address'
             {...register('address', {
               required: 'Please enter address',
               minLength: { value: 3, message: 'Address is more than 2 chars' },
             })}
           />
           {errors.address && (
-            <div className="text-red-500">{errors.address.message}</div>
+            <div className='text-red-500'>{errors.address.message}</div>
           )}
         </div>
-        <div className="mb-4">
-          <label htmlFor="city">City</label>
+        <div className='mb-4'>
+          <label htmlFor='city'>City</label>
           <input
-            className="w-full"
-            id="city"
+            className='w-full'
+            id='city'
             {...register('city', {
               required: 'Please enter city',
             })}
           />
           {errors.city && (
-            <div className="text-red-500 ">{errors.city.message}</div>
+            <div className='text-red-500 '>{errors.city.message}</div>
           )}
         </div>
-        <div className="mb-4">
-          <label htmlFor="postalCode">Postal Code</label>
+        <div className='mb-4'>
+          <label htmlFor='postalCode'>Postal Code</label>
           <input
-            className="w-full"
-            id="postalCode"
+            className='w-full'
+            id='postalCode'
             {...register('postalCode', {
               required: 'Please enter postal code',
             })}
           />
           {errors.postalCode && (
-            <div className="text-red-500 ">{errors.postalCode.message}</div>
+            <div className='text-red-500 '>{errors.postalCode.message}</div>
           )}
         </div>
-        <div className="mb-4">
-          <label htmlFor="country">Country</label>
+        <div className='mb-4'>
+          <label htmlFor='country'>Country</label>
           <input
-            className="w-full"
-            id="country"
+            className='w-full'
+            id='country'
             {...register('country', {
               required: 'Please enter country',
             })}
           />
           {errors.country && (
-            <div className="text-red-500 ">{errors.country.message}</div>
+            <div className='text-red-500 '>{errors.country.message}</div>
           )}
         </div>
-        <div className="mb-4 flex justify-between">
-          <button className="primary-button">Next</button>
+        <div className='mb-4 flex justify-between'>
+          <button className='primary-button'>Next</button>
         </div>
       </form>
     </Layout>
