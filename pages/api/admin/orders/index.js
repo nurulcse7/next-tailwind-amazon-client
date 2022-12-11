88b548661f8e98fa4a2ccef170965fac86334d4b
nurulcse7 <nurul.cse7@gmail.com>
2022-12-11 09:11:19 +0600

    Deliver-Order-For-Admin

--- pages/api/admin/orders/index.js
+++ pages/api/admin/orders/index.js
@@ -1,11 +1,11 @@
 import { getSession } from 'next-auth/react';
-import Order from '../../../models/Order';
-import db from '../../../utils/db';
+import Order from '../../../../models/Order';
+import db from '../../../../utils/db';
 
 const handler = async (req, res) => {
   const session = await getSession({ req });
   if (!session || (session && !session.user.isAdmin)) {
-    return res.status(401).send('Signin required');
+    return res.status(401).send('SignIn required');
   }
   if (req.method === 'GET') {
     await db.connect();
